dbac7edf47589a38c029c00e6f00a1b2d705a0d6
Chance <proxidius@gmail.com>
2022-09-18 00:33:45 -0600

    finished programmer showcase

---
+++ assets/js/hiddenCode.js
@@ -0,0 +1,11 @@
+
+
+(function $(){
+    
+    function ToggleCode(targetid) {
+    if(document.getElementById(targetid) .style.display=='none') 
+        {document.getElementById(targetid) .style.display=''}
+    else{
+        document.getElementById(targetid) .style.display='none'
+    }
+}})
